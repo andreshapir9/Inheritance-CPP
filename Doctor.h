d2974a84d92b1a2f2c003fc04192b9aab229d450
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-03 15:15:02 +0800

    Update Doctor.h

--- Doctor.h
+++ Doctor.h
@@ -1,15 +1,72 @@
-#ifndef Doctor_HEADER 
-#define Doctor_HEADER 
+/**  Doctor class inherited from Person class.
+*
+* #include "Doctor.h" <BR>
+* -llib
+*
+*/
+#ifndef DOCTOR_H
+#define DOCTOR_H
 
-#include "Person.h" 
+// SYSTEM INCLUDES
+#include<iostream>
 
-class Doctor :public Person {
-	char * Designation; 
-	int Salary; 
-public: 
-	Doctor(char * = NULL, int = 0, char = '\0', char * = NULL, int = 0);
-	void checkUp(); 
-	void prescribe();
+// LOCAL INCLUDES
+#include "Person.h"
+
+// Doctor class definition
+class Doctor : public Person {
+public:
+	// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	Doctor(const string& = "", int = 0, char = '/0', const string& = "", int = 0);
+
+	// Use compiler-generated copy constructor, assignment, and destructor.
+	// Doctor(const Doctor&);
+	// Doctor& operator=(const Doctor&);
+	// ~Doctor();
+
+	// OPERATIONS
+	/** function that depicts checking (patients) by Doctor.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void CheckUp();
+
+	/** function that depicts giving prescription by Doctor.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void Prescribe();
+
+
+// ACCESS
+	// setters
+	void SetSalary(int = 0);
+	void SetDesignation(const string& = "Doctor");
+	void SetDoctor(const string& = "", int = 0, char = '/0', int = 0);
+	/**
+	# @overload void SetDoctor(int = 0);
+	*/
+	void SetDoctor(int = 0);
+	/**
+	# @overload void SetDoctor(const Doctor& aDoctor);
+	*/
+	void SetDoctor(const Doctor& aDoctor);
+
+	// getters
+	int GetSalary()const;
+	const Doctor& GetDoctor()const;
+
+private:
+
+	// DATA MEMBERS
+	int mSalary;
 };
-
-#endif
+// end class Doctor
+#endif  
+// _DOCTOR_H_
