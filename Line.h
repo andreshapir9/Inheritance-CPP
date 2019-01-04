1bc88ccbaf321d3ccdf4764b36ead330270d560b
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-04 14:10:36 +0800

    Update Line.h

--- Line.h
+++ Line.h
@@ -1,13 +1,65 @@
-#ifndef _LINE_H 
-#define _LINE_H 
+/**  Line class inherited from Shape class.
+*
+* #include "Line.h" <BR>
+* -llib
+*
+*/
+#ifndef LINE_H
+#define LINE_H
 
-#include "Shape.h" 
+// SYSTEM INCLUDES
+#include<iostream>
 
-class Line :public Shape{ 
-	float mLength; 
-public: 
-	Line(char* = NULL, int = 0, float = 0.0); 
-	void Draw(); 
+// LOCAL INCLUDES
+#include "Shape.h"
+
+// Line class definition
+class Line : public Shape {
+public:
+
+// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	Line(const string& = "white", int = 0, float = 0.0);
+
+	// Use compiler-generated copy constructor, assignment, and destructor.
+	// Line(const Line&);
+	// Line& operator=(const Line&);
+	// ~Line();
+
+
+// OPERATIONS
+	/** Pure virtual function that draws Line.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void Draw();
+
+	
+// ACCESS
+	// setters
+	void SetLength(float = 0.0);
+	void SetLine(const string& = "white", int = 0, float = 0.0);
+	/**
+	# @overload void SetLine(float = 0.0);
+	*/
+	void SetLine(float = 0.0);
+	/**
+	# @overload void SetLine(const Line& aLine);
+	*/
+	void SetLine(const Line& aLine);
+
+	// getters
+	float GetLength()const;
+	const Line& GetLine()const;
+
+private:
+
+	// DATA MEMBERS
+	float mLength;
 };
-
-#endif
+// end class Line
+#endif  
+// _LINE_H_
